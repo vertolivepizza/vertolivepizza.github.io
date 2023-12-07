23c03d82b8eb7791b9536a60cf3852227f082dc4
= <=>
2023-12-07 11:56:42 +0100

    Votre message de commit

--- pizzaDuMois/description.js
+++ pizzaDuMois/description.js
@@ -1,4 +1,4 @@
 pizza = {
-    nom : "Pizza de novembre",
-    description : "En novembre venez découvrir cette recette de pâte à la compote de pomme, Tome du Ramier, Coppa, et noix. En partenariat avec la ferme de pellausy et la ferme du ramier."
-}
+  "nom": "Pizza de décembre",
+  "description": "Vert Olive Pizza vous gâte en décembre.\r\n\r\nUne explosion de saveurs sur cette pizza du mois. Du confit d'oignons pour la douceur, du Brie à la Truffe pour le caractère, du magret séché pour la fête, de la roquette et des tomates cerise pour rappeler Noël et pour finir un léger filet d'huile de pépins de raisin aromatisé à la Truffe. \r\n\r\nC'est une première pour vert olive de travailler ce genre de produits et c'est un réussite. \r\nHâte de vous faire découvrir."
+};
