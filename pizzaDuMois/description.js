76023658000a1f6865954a090c8a3d14e363f855
aldsanms <aldryckgta@gmail.com>
2024-01-04 22:50:49 +0100

    maj

--- pizzaDuMois/description.js
+++ pizzaDuMois/description.js
@@ -1,4 +1,4 @@
 pizza = {
-  "nom": "Pizza de décembre",
-  "description": "Vert Olive Pizza vous gâte en décembre.\r\n\r\nUne explosion de saveurs sur cette pizza du mois. Du confit d'oignons pour la douceur, du Brie à la Truffe pour le caractère, du magret séché pour la fête, de la roquette et des tomates cerise pour rappeler Noël et pour finir un léger filet d'huile de pépins de raisin aromatisé à la Truffe. \r\n\r\nC'est une première pour vert olive de travailler ce genre de produits et c'est un réussite. \r\nHâte de vous faire découvrir."
+  "nom": "Pizza de janvier",
+  "description": "C'est sur une note de \"raclette\" que Vet Olive Pizza vous propose de comencer l'année.<br><br>En janvier découvrez le mariage de : Confit d'oignons, morbier, pancetta, champignons.<br><br>Histoire de rester dans l'sprit gourmand des fêtes!<br><br> À bientôt"
 };
