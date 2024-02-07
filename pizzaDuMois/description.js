76d43a1ce0375aee92d34569c25ca4fee02937d7
= <=>
2024-02-07 11:07:16 +0100

    Votre message de commit

--- pizzaDuMois/description.js
+++ pizzaDuMois/description.js
@@ -1,4 +1,4 @@
 pizza = {
   "nom": "Pizza de janvier",
-  "description": "C'est sur les couleurs de la St Valentin que Vert Olive Pizza s'est inspiré:\r\n\r\nUne pâte colorée en rose, une base en purée de betteraves, du fromage de chévre frais, de la roquette, bresaola, éclats de pistaches et un filet de crème balsamique à la framboise.\r\n\r\nÀ bientôt.."
+  "description": "C'est sur les couleurs de la St Valentin que Vert Olive Pizza s'est inspiré:\r\n\r\nUne pâte colorée en rose, une base en purée de betteraves, du fromage de chèvre frais, de la roquette, bresaola, éclats de pistaches et un filet de crème balsamique à la framboise.\r\n\r\nÀ bientôt."
 };
