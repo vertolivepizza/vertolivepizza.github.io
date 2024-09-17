e52403182a94de311c46a54bb4118a38f25c2736
aldsanms <aldryckgta@gmail.com>
2024-09-17 18:56:38 +0200

    maj

--- pizzaDuMois/description.js
+++ pizzaDuMois/description.js
@@ -1,7 +1,6 @@
 pizza = {
-  "nom": "Pizza de juillet",
-  "description": "La pizza de juillet est là chez vert olive pizza <br><br>" +
-"Inspirée par les saveurs estivales : <br>" +
-"&nbsp Base crème d'ail, camembert, herbes de Provence, noix et pancetta<br><br>"+
-"Un délice pour les papilles !"
+  "nom": "Pizza de Septembre",
+  "description": "Et on redémarre l'année avec une création tirée de mes vacances en Galice.<br>"+
+                  "Une base tomate, gorgonzola, jambon sec d'Espagne, roquette, confiture de figues, noisettes torréfiées... olé ! <br><br>"+
+                  "L'été n'est pas fini chez vert olive pizza..."
 };
