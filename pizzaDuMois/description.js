e4bd6bea4904613d842cd216e57ac6503961b0e3
aldsanms <aldryckgta@gmail.com>
2024-11-27 16:38:32 +0100

    maj

--- pizzaDuMois/description.js
+++ pizzaDuMois/description.js
@@ -1,7 +1,4 @@
 pizza = {
-  "nom": "Pizza d'Octobre",
-  "description": "Chez Vert Olive Pizza, laissez-vous séduire par notre pizza spéciale d'octobre! <br><br>"+
-  "Une harmonie de saveurs automnales avec une base fondante de butternut, agrémentée de poitrine fumée pour une touche intense, des marrons croquants et des pétales de parmesan délicatement posés pour sublimer chaque bouchée.<br><br>"+ 
-  "Une invitation gourmande qui célèbre l'automne avec élégance et générosité. <br><br>"+
-  "Venez vite la déguster chez Vert Olive Pizza !"
+  "nom": "Pizza de Novembre",
+  "description": "La pizza de novembre est arrivée! <br> <br>Avec ses oignons confits, champignons de Paris, la scamorza fumée et son jambon sec... <br> <br>un régal pour les papilles"
 };
