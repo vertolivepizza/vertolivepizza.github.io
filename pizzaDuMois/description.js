69f58f44da69da537e716ba1e6b005882c4ca6d8
aldsanms <aldryckgta@gmail.com>
2025-09-26 21:36:45 +0200

    fin

--- pizzaDuMois/description.js
+++
@@ -1,4 +0,0 @@
-pizza = {
-  "nom": "Pizza de décembre",
-  "description": "La pizza de décembre est arrivée chez Vert Olive Pizza. <br> <br>Base confit d'oignons, Brie truffé, magrets séché, roquette, tomates cerises, filet d'huile parfumé à la Truffe. <br> <br>À bientôt chez Vert Olive Pizza !"
-};
